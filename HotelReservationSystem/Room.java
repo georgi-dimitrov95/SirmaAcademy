ed4376e7150921a4344078edcffe37e270f492d1
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2024-07-18 18:52:12 +0300

    create getters & setters for Room class properties

--- HotelReservationSystem/Room.java
+++ HotelReservationSystem/Room.java
@@ -5,8 +5,65 @@ public class Room {
     String type;
     String amenities;
     Double price;
+    int maxOccupancy;
     int cancelFeePercent;
     boolean isAvailable;
+
+    public int getMaxOccupancy() {
+        return maxOccupancy;
+    }
+
+    public void setMaxOccupancy(int maxOccupancy) {
+        this.maxOccupancy = maxOccupancy;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getAmenities() {
+        return amenities;
+    }
+
+    public void setAmenities(String amenities) {
+        this.amenities = amenities;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public int getCancelFeePercent() {
+        return cancelFeePercent;
+    }
+
+    public void setCancelFeePercent(int cancelFeePercent) {
+        this.cancelFeePercent = cancelFeePercent;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
 }
 
 
