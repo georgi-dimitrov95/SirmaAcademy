0de3b576cc60e5d082a62419506d271b7d9a5c5a
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2024-07-18 17:33:35 +0300

    define properties for Room class

---
+++ HotelReservationSystem/Room.java
@@ -0,0 +1,22 @@
+package HotelReservationSystem;
+
+public class Room {
+    String number;
+    String type;
+    String amenities;
+    Double price;
+    int cancelFeePercent;
+    boolean isAvailable;
+}
+
+
+
+
+
+// Create a file for room instances: e.g., Deluxe Room 101, Suite 201.
+//Attributes for each room should include:
+        //Room number
+        //Type (e.g., Deluxe, Suite)
+        //Price per night
+        //Cancellation fee
+        //Status (e.g., available, booked)
