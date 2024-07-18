9c440a37be7841e22c6b2dbc2a8847c86ee55960
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2024-07-18 20:17:07 +0300

    define properties for Hotel class

---
+++ HotelReservationSystem/Hotel.java
@@ -0,0 +1,14 @@
+package HotelReservationSystem;
+
+import java.util.ArrayList;
+
+public class Hotel {
+    double balance;
+    String name;
+    String address;
+    String phoneNumber;
+    String website;
+    String utilities;
+    int floors;
+    ArrayList<ArrayList<Room>> rooms;
+}
