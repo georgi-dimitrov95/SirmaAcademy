071ff63d2d66d1572dfd4186ee4fb72e428f6460
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2024-07-19 01:09:39 +0300

    the initial menu that greets the user upon starting the program

---
+++ HotelReservationSystem/Menu.java
@@ -0,0 +1,12 @@
+package HotelReservationSystem;
+
+public class Menu {
+
+//    greets the user upon starting the program
+    public static void GreetingMenu() {
+        String welcome = "Welcome to the Hotel Reservation System!";
+        System.out.println("=".repeat(welcome.length()));
+        System.out.println(welcome);
+        System.out.println("=".repeat(welcome.length()));
+    }
+}
