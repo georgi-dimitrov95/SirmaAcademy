8281cf90bdd6c948afefc649b5dc2ad5dceabed2
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2024-07-19 23:17:07 +0300

    create methods for adding & deleting users & hotels

--- HotelReservationSystem/ReservationSystem.java
+++ HotelReservationSystem/ReservationSystem.java
@@ -21,4 +21,16 @@ public class ReservationSystem {
     public void setHotels(ArrayList<Hotel> hotels) {
         this.hotels = hotels;
     }
+
+    public void addUser(User user) {
+        users.add(user);
+    }
+
+    public void addHotel(Hotel hotel) {
+        hotels.add(hotel);
+    }
+
+    public void deleteHotel(Hotel hotel) {
+        hotels.remove(hotel);
+    }
 }
