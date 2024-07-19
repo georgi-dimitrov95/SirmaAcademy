cff307c2a71603c918118d3d6227073f1fe6a1ca
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2024-07-19 23:14:42 +0300

    create properties, getters & setters for ReservationSystem class

---
+++ HotelReservationSystem/ReservationSystem.java
@@ -0,0 +1,24 @@
+package HotelReservationSystem;
+
+import java.util.ArrayList;
+
+public class ReservationSystem {
+    ArrayList<Hotel> hotels;
+    ArrayList<User> users;
+
+    public ArrayList<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(ArrayList<User> users) {
+        this.users = users;
+    }
+
+    public ArrayList<Hotel> getHotels() {
+        return hotels;
+    }
+
+    public void setHotels(ArrayList<Hotel> hotels) {
+        this.hotels = hotels;
+    }
+}
