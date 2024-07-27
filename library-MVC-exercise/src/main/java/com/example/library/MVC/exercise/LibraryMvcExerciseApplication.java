d199ee85b578ea77f9bc7a2421bc6647f3f8da51
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2024-07-28 00:30:37 +0300

    initialize app as a Maven project with Spring Boot

---
+++ library-MVC-exercise/src/main/java/com/example/library/MVC/exercise/LibraryMvcExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.library.MVC.exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryMvcExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryMvcExerciseApplication.class, args);
+	}
+
+}
