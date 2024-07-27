8fe277fa2b37a6b8ac5e70cb797d43a4a123d044
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2024-07-28 00:45:54 +0300

    create getters & setters for Book class properties

--- library-MVC-exercise/src/main/java/com/example/library/MVC/exercise/model/Book.java
+++ library-MVC-exercise/src/main/java/com/example/library/MVC/exercise/model/Book.java
@@ -18,4 +18,44 @@ public class Book {
     public Book() {
 
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
 }
