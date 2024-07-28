1b0b19b3a4bc8cb280e93cc85465698e4a17e40e
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2024-07-29 00:43:00 +0300

    create post mapping method which adds a book to the list and then reloads the page by redirecting to it

--- library-MVC-exercise/src/main/java/com/example/library/MVC/exercise/controller/LibraryController.java
+++ library-MVC-exercise/src/main/java/com/example/library/MVC/exercise/controller/LibraryController.java
@@ -1,11 +1,14 @@
 package com.example.library.MVC.exercise.controller;
 
 
+import com.example.library.MVC.exercise.model.Book;
 import com.example.library.MVC.exercise.service.LibraryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LibraryController {
@@ -15,6 +18,13 @@ public class LibraryController {
     @GetMapping("/books")
     public String viewAllBooks(Model model) {
         model.addAttribute("books", libraryService.getAllBooks());
+        model.addAttribute("book", new Book());
         return "books";
+    }
+
+    @PostMapping("/books")
+    public String addBook(@ModelAttribute Book book) {
+        libraryService.addBook(book);
+        return "redirect:/books";
     }
 }
