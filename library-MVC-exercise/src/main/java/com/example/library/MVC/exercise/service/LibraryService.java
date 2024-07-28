25ab1f8d3043101bde4391f1907c09de2d1a970c
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2024-07-28 17:45:47 +0300

    create method which returns a list of all books

--- library-MVC-exercise/src/main/java/com/example/library/MVC/exercise/service/LibraryService.java
+++ library-MVC-exercise/src/main/java/com/example/library/MVC/exercise/service/LibraryService.java
@@ -1,9 +1,18 @@
 package com.example.library.MVC.exercise.service;
 
+import com.example.library.MVC.exercise.model.Book;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.ArrayList;
 
 @Service
 public class LibraryService {
+    @Autowired
     private CsvService csvService;
-        
+
+    public ArrayList<Book> getAllBooks() {
+        return csvService.readAllBooks();
+    }
+
+
 }
