9de37ff118c012ed0204c8f4d09343d8db3bc5e4
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2024-07-28 23:32:29 +0300

    create LibraryController class to handle requests from the client

---
+++ library-MVC-exercise/src/main/java/com/example/library/MVC/exercise/controller/LibraryController.java
@@ -0,0 +1,14 @@
+package com.example.library.MVC.exercise.controller;
+
+
+import com.example.library.MVC.exercise.service.LibraryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LibraryController {
+    @Autowired
+    private LibraryService libraryService;
+}
