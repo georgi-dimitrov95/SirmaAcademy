4bcd25efe6bc00a62710884c7a58a92dbadac028
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2024-08-01 00:47:33 +0300

    create getters & setters for class fields

--- restdbapp/src/main/java/models/Person.java
+++ restdbapp/src/main/java/models/Person.java
@@ -14,4 +14,28 @@ public class Person {
     public Person() {
 
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
