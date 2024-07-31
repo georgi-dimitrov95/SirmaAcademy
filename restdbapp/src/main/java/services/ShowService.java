9ae1b3d6fe0c55df9f3f682ff7131a4ab5a3d62b
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2024-08-01 01:47:11 +0300

    create method that validates Show data and adds the Show to the db if the data is valid

--- restdbapp/src/main/java/services/ShowService.java
+++ restdbapp/src/main/java/services/ShowService.java
@@ -1,5 +1,6 @@
 package services;
 
+import models.Show;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import repositories.ShowRepository;
@@ -12,5 +13,10 @@ public class ShowService {
 
     public ShowService(ShowRepository showRepository) {
         this.showRepository = showRepository;
+    }
+
+    public void registerShow(Show show) {
+//        TODO data validation
+        showRepository.saveShowToDatabase(show);
     }
 }
