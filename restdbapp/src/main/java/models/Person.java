a13e336aa6d532ed37a40cd5f4a0a2857ac36df1
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2024-08-01 00:47:02 +0300

    create parametrized & empty constructors

--- restdbapp/src/main/java/models/Person.java
+++ restdbapp/src/main/java/models/Person.java
@@ -4,4 +4,14 @@ public class Person {
     private int id;
     private String name;
     private int age;
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person() {
+
+    }
 }
