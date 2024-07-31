4777ab89c5d1726fc47bdcc9f9b54f706cd7887b
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2024-08-01 01:31:31 +0300

    create ShowRepository class with autowired jdbcTemplate and a constructor

---
+++ restdbapp/src/main/java/repositories/ShowRepository.java
@@ -0,0 +1,16 @@
+package repositories;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ShowRepository {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public ShowRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+}
