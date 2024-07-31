0bb7427a10a0531063893796fcbee87564227517
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2024-08-01 00:58:57 +0300

    create getters & setters for class fields

--- restdbapp/src/main/java/models/Show.java
+++ restdbapp/src/main/java/models/Show.java
@@ -14,6 +14,38 @@ public class Show {
     }
 
     public Show() {
-        
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getEpisodes() {
+        return episodes;
+    }
+
+    public void setEpisodes(int episodes) {
+        this.episodes = episodes;
     }
 }
