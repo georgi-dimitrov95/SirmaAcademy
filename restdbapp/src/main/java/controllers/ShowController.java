ae671359e17c233fc2a440b344876d8636b9ae13
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2024-08-01 02:33:47 +0300

    create a post mapping method that registers a show

--- restdbapp/src/main/java/controllers/ShowController.java
+++ restdbapp/src/main/java/controllers/ShowController.java
@@ -1,6 +1,9 @@
 package controllers;
 
+import models.Show;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import repositories.ShowRepository;
@@ -13,5 +16,17 @@ public class ShowController {
 
     public ShowController(ShowRepository showRepository) {
         this.showRepository = showRepository;
+    }
+
+    @PostMapping("/register")
+    public String register(@RequestBody Show show) {
+//        TODO validate if needed data is provided
+        try {
+            showRepository.saveShowToDatabase(show);
+        } catch (RuntimeException e) {
+            System.out.println("Ran into an error while registering show.");
+            throw  new RuntimeException(e);
+        }
+        return "Successfully registered the show";
     }
 }
