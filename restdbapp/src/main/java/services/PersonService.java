eed0712da9b76d41d961b2f711513e4b2f9595f9
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2024-08-01 01:44:10 +0300

    create method that validates Person data and registers the Person in the db if the data is valid

--- restdbapp/src/main/java/services/PersonService.java
+++ restdbapp/src/main/java/services/PersonService.java
@@ -1,5 +1,6 @@
 package services;
 
+import models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import repositories.PersonRepository;
@@ -12,5 +13,10 @@ public class PersonService {
 
     public PersonService(PersonRepository personRepository) {
         this.personRepository = personRepository;
+    }
+
+    public void registerPerson(Person person) {
+//        TODO data validation & password encryption
+        personRepository.saveUserInDatabase(person);
     }
 }
