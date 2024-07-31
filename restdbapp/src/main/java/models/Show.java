876b0afed90291871ea612a247bdb457eb0bc633
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2024-08-01 00:58:27 +0300

    create parametrized & empty constructors

--- restdbapp/src/main/java/models/Show.java
+++ restdbapp/src/main/java/models/Show.java
@@ -5,4 +5,15 @@ public class Show {
     private String title;
     private int year;
     private int episodes;
+
+    public Show(int id, String title, int year, int episodes) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.episodes = episodes;
+    }
+
+    public Show() {
+        
+    }
 }
