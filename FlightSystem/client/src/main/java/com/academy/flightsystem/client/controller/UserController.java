50d493c332c0cabad3fde9a97da3792826d0aca4
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2024-08-11 01:59:46 +0300

    create PostMapping for user registration

--- FlightSystem/client/src/main/java/com/academy/flightsystem/client/controller/UserController.java
+++ FlightSystem/client/src/main/java/com/academy/flightsystem/client/controller/UserController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
@@ -16,5 +18,18 @@ public class UserController {
     @GetMapping("/register")
     public String registerPage(Model model) {
         model.addAttribute("user", new User());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute User user, Model model) {
+        try {
+            userService.register(user);
+            return "redirect:/login";
+        } catch (Exception e) {
+            model.addAttribute("user", new User());
+            model.addAttribute("error", "An error occurred during the registration.");
+            return "register";
+        }
     }
 }
