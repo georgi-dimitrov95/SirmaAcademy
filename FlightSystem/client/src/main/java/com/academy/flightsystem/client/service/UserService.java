85ea054057dc92313460cf1037f55ac3e9460b4c
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2024-08-10 22:52:33 +0300

    create register method

--- FlightSystem/client/src/main/java/com/academy/flightsystem/client/service/UserService.java
+++ FlightSystem/client/src/main/java/com/academy/flightsystem/client/service/UserService.java
@@ -1,11 +1,21 @@
 package com.academy.flightsystem.client.service;
 
+import com.academy.flightsystem.client.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class UserService {
 
     @Value("$backend.api.url")
     private String backendApiUrl;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public void register(User user) {
+        restTemplate.postForObject(backendApiUrl + "/users/register", user, User.class);
+    }
 }
