9d9b287088dbb2e78660d19f4815aea4662bc3f0
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2024-08-12 01:29:19 +0300

    create User class

--- FlightSystem/client/src/main/java/com/academy/flightsystem/client/model/User.java
+++ FlightSystem/client/src/main/java/com/academy/flightsystem/client/model/User.java
@@ -1,4 +1,13 @@
 package com.academy.flightsystem.client.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+    private String username;
+    private String password;
 }
