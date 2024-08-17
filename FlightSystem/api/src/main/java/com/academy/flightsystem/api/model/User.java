c0ef51507ac2d5be08390c4add4ef0ad93074b2a
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2024-08-18 00:51:29 +0300

    create class

---
+++ FlightSystem/api/src/main/java/com/academy/flightsystem/api/model/User.java
@@ -0,0 +1,23 @@
+package com.academy.flightsystem.api.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    @ManyToMany
+    private Set<Role> roles;
+}
