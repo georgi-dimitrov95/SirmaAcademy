04347de8985cf046e2afece00b52406f4ad988f1
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2024-08-28 15:21:15 +0300

    rewrite 'roles' field and add DB logic

--- FlightSystem/api/src/main/java/com/academy/flightsystem/api/model/UserInfo.java
+++ FlightSystem/api/src/main/java/com/academy/flightsystem/api/model/UserInfo.java
@@ -5,7 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @Data
@@ -18,7 +20,12 @@ public class UserInfo {
     private Long id;
     private String username;
     private String password;
-    private String roles = "USER";
-//    @OneToMany
-//    private List<Ticket> tickets;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
 }
