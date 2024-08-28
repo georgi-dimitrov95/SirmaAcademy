5d8448e294d8bb74f1bae9e4f74b4661151c7f0b
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2024-08-28 18:34:44 +0300

    add bean for UserInfoService

--- FlightSystem/api/src/main/java/com/academy/flightsystem/api/security/SecurityConfig.java
+++ FlightSystem/api/src/main/java/com/academy/flightsystem/api/security/SecurityConfig.java
@@ -1,12 +1,17 @@
 package com.academy.flightsystem.api.security;
 
 import com.academy.flightsystem.api.filters.JwtAuthenticationFilter;
+import com.academy.flightsystem.api.model.UserInfo;
+import com.academy.flightsystem.api.service.UserInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,6 +20,13 @@ public class SecurityConfig {
 
     @Autowired
     private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserInfoService();
+    }
+
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
