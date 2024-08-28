f83851e8971fe0e46aed1ddc9f0b748fa912fbda
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2024-08-28 18:30:18 +0300

    modify filterChain method

--- FlightSystem/api/src/main/java/com/academy/flightsystem/api/security/SecurityConfig.java
+++ FlightSystem/api/src/main/java/com/academy/flightsystem/api/security/SecurityConfig.java
@@ -1,22 +1,30 @@
 package com.academy.flightsystem.api.security;
 
+import com.academy.flightsystem.api.filters.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
-                                .requestMatchers("/auth/login", "/auth/register", "/").permitAll()
-                                .requestMatchers("/auth/admin").hasRole("ROLE_ADMIN")
-                                .anyRequest().authenticated()
+                                .requestMatchers("/auth/**", "/").permitAll()
+                                .requestMatchers("/auth/admin").hasRole("ADMIN")
+                                .requestMatchers("/auth/user").hasRole("USER")
+                                .anyRequest().authenticated() // Requires authentication for all other requests
                 );
         return http.build();
     }
