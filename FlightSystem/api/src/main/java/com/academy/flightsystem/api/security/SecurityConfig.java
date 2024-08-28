3485a80371859464b460710034e6c80a2dec27cc
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2024-08-28 18:36:23 +0300

    add bean for AuthenticationManager

--- FlightSystem/api/src/main/java/com/academy/flightsystem/api/security/SecurityConfig.java
+++ FlightSystem/api/src/main/java/com/academy/flightsystem/api/security/SecurityConfig.java
@@ -6,8 +6,10 @@ import com.academy.flightsystem.api.service.UserInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -26,6 +28,11 @@ public class SecurityConfig {
         return new UserInfoService();
     }
 
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+    
 
 
     @Bean
