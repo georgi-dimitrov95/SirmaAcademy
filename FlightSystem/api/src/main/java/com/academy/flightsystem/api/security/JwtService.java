fc807a18bdbeba1309e6261cd29f0745224906af
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2024-08-28 21:05:16 +0300

    add getSignKey method for key encryption

--- FlightSystem/api/src/main/java/com/academy/flightsystem/api/security/JwtService.java
+++ FlightSystem/api/src/main/java/com/academy/flightsystem/api/security/JwtService.java
@@ -1,22 +1,39 @@
 package com.academy.flightsystem.api.security;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+// JwtService is responsible for generating, validating and extracting info from JWTs.
+// It handles core operations like creating & extracting claims and validating tokens against user details.
 @Service
 public class JwtService {
 
+    @Getter
     @Value("${jwt.secret}")
     private String key;
 
+    @Getter
     @Value("${jwt.expiration}")
     private Long expiration;
 
-    public void generateToken(String username) {
+    private SecretKey getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(getKey());
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
 
+    public String generateToken(String username) {
+        return "";
     }
 
     public boolean isTokenValid(String token) {
         return true;
     }
+
 }
