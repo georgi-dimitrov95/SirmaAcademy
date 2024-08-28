8e58243a03ba5b1a33ffcdf41d6d3fbe844ea217
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2024-08-28 15:37:06 +0300

    create interface

--- FlightSystem/api/src/main/java/com/academy/flightsystem/api/repository/UserRepository.java
+++ FlightSystem/api/src/main/java/com/academy/flightsystem/api/repository/UserRepository.java
@@ -1,7 +1,12 @@
 package com.academy.flightsystem.api.repository;
 
-import com.academy.flightsystem.api.model.User;
+import com.academy.flightsystem.api.model.UserInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserInfo, Long> {
+    public Optional<UserInfo> findByUsername(String username);
 }
