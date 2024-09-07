f64df7d8fd48f4aeec277f21b6b45cf97a1ee7c4
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2024-09-07 20:13:13 +0300

    initialize app with csv files included

---
+++ FootballApp/api/src/main/java/com/football/api/FootballStatsApiApplication.java
@@ -0,0 +1,13 @@
+package com.football.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballStatsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballStatsApiApplication.class, args);
+	}
+
+}
