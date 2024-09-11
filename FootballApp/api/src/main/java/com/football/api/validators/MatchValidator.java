f3f8e3b576cbd351137dfcfa19ae8c0269d72cde
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2024-09-11 14:14:35 +0300

    create class that validates data from csv files containing Match data

---
+++ FootballApp/api/src/main/java/com/football/api/validators/MatchValidator.java
@@ -0,0 +1,26 @@
+package com.football.api.validators;
+
+public class MatchValidator implements ValidatorCsv{
+
+    @Override
+    public boolean dataIsValid(String[] fieldsRow) {
+
+//        csv files for teams should have 5 columns
+        if (fieldsRow.length != 5) {
+            return false;
+        }
+
+//        skip the first element (containing ID) because it's not needed
+        try {
+            Long aID = Long.parseLong(fieldsRow[1]);
+            Long bID = Long.parseLong(fieldsRow[2]);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+
+        String date = fieldsRow[3];
+        String score = fieldsRow[4];
+
+        return StringValidator.validateDate(date) && StringValidator.validateScore(score);
+    }
+}
