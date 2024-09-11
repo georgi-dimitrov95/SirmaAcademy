e65238b8d3411dde810fa5b1a4073d9bf1c5651f
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2024-09-11 23:59:52 +0300

    add repository methods using JPA query keywords

--- FootballApp/api/src/main/java/com/football/api/repository/jpa/PlayerJpaRepository.java
+++ FootballApp/api/src/main/java/com/football/api/repository/jpa/PlayerJpaRepository.java
@@ -4,6 +4,12 @@ import com.football.api.model.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface PlayerJpaRepository extends JpaRepository<Player, Long> {
+    @Override
+    List<Player> findAll();
 }
--- FootballApp/api/src/main/java/com/football/api/repository/jpa/RecordJpaRepository.java
+++ FootballApp/api/src/main/java/com/football/api/repository/jpa/RecordJpaRepository.java
@@ -4,6 +4,10 @@ import com.football.api.model.Record;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RecordJpaRepository extends JpaRepository<Record, Long> {
+    List<Record> findByPlayerId(Long id);
+    List<Record> findByMatchId(Long id);
 }
