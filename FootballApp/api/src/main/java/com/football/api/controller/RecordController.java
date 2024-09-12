4914038689afd281857c4f08d2c261b1d5a0c708
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2024-09-12 04:04:22 +0300

    operation that gets a RecordDto by ID

--- FootballApp/api/src/main/java/com/football/api/controller/RecordController.java
+++ FootballApp/api/src/main/java/com/football/api/controller/RecordController.java
@@ -2,8 +2,12 @@ package com.football.api.controller;
 
 import com.football.api.model.Match;
 import com.football.api.model.Record;
+import com.football.api.model.dto.RecordDto;
 import com.football.api.service.RecordService;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -26,7 +30,20 @@ public class RecordController {
         }
     }
 
+    @GetMapping("/get")
     public List<Record> getAllRecords() {
         return recordService.getAllRecords();
     }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<?> getRecord(@PathVariable Long id) {
+        try {
+            RecordDto recordDto = recordService.getRecord(id);
+            return ResponseEntity.ok(recordDto);
+        } catch (EntityNotFoundException e) {
+            String errorMessage = "Record not found with ID: " + id;
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMessage);
+        }
+    }
+
 }
--- FootballApp/api/src/main/java/com/football/api/service/RecordService.java
+++ FootballApp/api/src/main/java/com/football/api/service/RecordService.java
@@ -1,8 +1,10 @@
 package com.football.api.service;
 
 import com.football.api.model.Record;
+import com.football.api.model.dto.RecordDto;
 import com.football.api.repository.csv.RecordCsvReader;
 import com.football.api.repository.jpa.RecordJpaRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -31,5 +33,10 @@ public class RecordService {
 
     public List<Record> getAllRecords() {
         return recordJpaRepository.findAll();
+    }
+
+    public RecordDto getRecord(Long id) {
+        Record record = recordJpaRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+        return new RecordDto(record);
     }
 }
