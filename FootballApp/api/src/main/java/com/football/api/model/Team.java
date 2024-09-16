dbdfeabc746f6a5d65803bbd12f93349b8523369
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2024-09-17 02:40:01 +0300

    add annotations for validating a Team entity

--- FootballApp/api/src/main/java/com/football/api/model/Team.java
+++ FootballApp/api/src/main/java/com/football/api/model/Team.java
@@ -1,6 +1,9 @@
 package com.football.api.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,8 +16,16 @@ public class Team {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "The name of the team should neither be null nor an empty string")
+    @Pattern(regexp = "^'?\\p{L}+(['`’ -]\\p{L}+)*(\\p{Zs}\\(\\p{L}+\\))?$", message = "Invalid team name")
     private String name;
+
+    @NotBlank(message = "The name of the manager should neither be null nor an empty string")
+    @Pattern(regexp = "^'?\\p{L}+(['`’ -]\\p{L}+)*(\\p{Zs}\\(\\p{L}+\\))?$", message = "Invalid manager name")
     private String manager;
+
+    @Pattern(regexp = "^[A-Z]?$", message = "The group can be either a single uppercase letter or just an empty string")
     private String tournamentGroup;
 
     public Team(String[] fieldsRow) {
