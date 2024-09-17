746b1e2fbfb2d84bf9e3bbe4a273bbdeeece477b
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2024-09-17 03:09:43 +0300

    add annotations for validating a Player entity

--- FootballApp/api/src/main/java/com/football/api/model/Player.java
+++ FootballApp/api/src/main/java/com/football/api/model/Player.java
@@ -1,6 +1,10 @@
 package com.football.api.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,8 +17,17 @@ public class Player {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Min(value = 0, message = "Team number should not be less than 0")
+    @Max(value = 99, message = "Team number should not be greater than 99")
     private int teamNumber;
+
+    @NotBlank(message = "The position should neither be null nor an empty string")
+    @Pattern(regexp = "^(GK|DF|MF|FW)$", message = "Invalid position. Allowed ones: GK, DF, MF, FW")
     private String position;
+
+    @NotBlank(message = "The name of the player should neither be null nor an empty string")
+    @Pattern(regexp = "^'?\\p{L}+(['`’ -]\\p{L}+)*(\\p{Zs}\\(\\p{L}+\\))?$", message = "Invalid player name")
     private String fullName;
 
     @ManyToOne
