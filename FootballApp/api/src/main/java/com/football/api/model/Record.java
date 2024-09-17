e1cb8baa2e6006c24dab26b6170bea8c3d36ec4a
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2024-09-17 03:39:02 +0300

    add annotations for validating a Record entity

--- FootballApp/api/src/main/java/com/football/api/model/Record.java
+++ FootballApp/api/src/main/java/com/football/api/model/Record.java
@@ -1,6 +1,9 @@
 package com.football.api.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,12 +19,20 @@ public class Record {
 
     @ManyToOne
     @JoinColumn(name="player_id")
+    @NotNull(message = "Player should not be null")
     private Player player;
 
     @ManyToOne
     @JoinColumn(name="match_id")
+    @NotNull(message = "Match should not be null")
     private Match match;
 
+//    further validation for both minutes fields in the Service class is needed
+    @Min(value = 0, message = "Starting minutes should not be less than 0")
+    @Max(value = 120, message = "Starting minutes should not be greater than 120")
     private int fromMinutes;
+
+    @Min(value = 1, message = "End minutes should not be less than 1")
+    @Max(value = 120, message = "End minutes should not be greater than 120")
     private int toMinutes;
 }
