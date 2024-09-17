644cf809ef5bb62dc3c5e3272d7209b31e028247
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2024-09-17 03:30:56 +0300

    add annotations for validating a Match entity

--- FootballApp/api/src/main/java/com/football/api/model/Match.java
+++ FootballApp/api/src/main/java/com/football/api/model/Match.java
@@ -1,6 +1,8 @@
 package com.football.api.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,13 +20,20 @@ public class Match {
 
     @ManyToOne
     @JoinColumn(name="a_team_id")
+    @NotNull(message = "Team A should not be null")
     private Team aTeam;
 
     @ManyToOne
     @JoinColumn(name="b_team_id")
+    @NotNull(message = "Team B should not be null")
     private Team bTeam;
 
+//    should be changed to a date class or go through further validation in the Service class
+    @NotBlank(message = "The date should neither be null nor an empty string")
     private String date;
+
+//    further validation in the Service class
+    @NotBlank(message = "The score should neither be null nor an empty string")
     private String score;
 
     public Match(Team aTeam, Team bTeam, String[] fieldsRow) {
